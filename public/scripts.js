ea70f35ae3df01262ef49fea3b36ffba8f6de1f4
Sigrid Jørgensen Bakke <sigrid.j.bakke@gmail.com>
2017-03-09 20:53:08 +0100

    Put script in .js file

--- public/scripts.js
+++ public/scripts.js
@@ -6,6 +6,31 @@ function toggleMenuMobile() {
     visibleMenu = !visibleMenu;
 }
 
+// Make count down great again
+var countDownDate = new Date("Jul 29, 2017 14:30:00").getTime();
+// Update count down every second:
+var x = setInterval(function() {
+    // Todays date and time:
+    var now = new Date().getTime();
+    // Time between today and wedding:
+    var dist = countDownDate - now;
+    // Separate in days, hours, minutes and seconds:
+    var days = Math.floor(dist / (1000*60*60*24));
+    var hours = Math.floor((dist % (1000*60*60*24)) / (1000*60*60));
+    var min = Math.floor((dist % (1000*60*60)) / (1000*60));
+    var sec = Math.floor((dist % (1000*60)) / 1000);
+
+    // Display count down:
+    document.getElementById("countdown").innerHTML = "Om ganske nøyaktig " + days + " dager, " + hours + " timer, " + min + " minutter og " + sec + " sekunder.";
+
+    //If count down is finished:
+    if (dist<0) {
+        clearInterval(x);
+        document.getEementById("countdown").innerHTML = "Nå er vi gift!";
+    }
+}, 1000);
+
+
 // Used to toggle the content blocks
 function toggleSection(id) {
     var element = document.getElementById(id).getElementsByClassName("inner")[0];
